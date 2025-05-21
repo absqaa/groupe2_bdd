af5f90aa4fab41ddaab619e26009dd4bb7cd7aeb
ishy <ishaanmundols@gmail.coom>
2025-05-21 17:25:19 +0200

    Requests fix

--- requests.sql
+++ requests.sql
@@ -1,10 +1,69 @@
+--- 5.1.1
 SELECT
-  NomComplet,
-  DATE_naissance
+    m.nom AS NomMedicament,
+    COUNT(pr.id_prescription) AS NombreDeUtilisations
+FROM Médicament AS m
+JOIN Commande as cm on m.id_médicament = cm.id_médicament
+JOIN Prescription AS pr ON cm.id_prescription = pr.id_prescription
+GROUP BY m.nom
+ORDER BY NombreDeUtilisations DESC;
+
+
+--- 5.1.2
+SELECT
+    m.nom AS NomMedicament,
+    COUNT(pr.id_prescription) AS NombreDeUtilisations
+FROM Médicament AS m
+JOIN Commande as cm on m.id_médicament = cm.id_médicament
+JOIN Prescription AS pr ON cm.id_prescription = pr.id_prescription
+GROUP BY m.nom
+ORDER BY NombreDeUtilisations DESC;
+
+
+--- 5.1.3
+SELECT
+    p.NomComplet,
+    SUM(f.montant) AS MontantàPayer,
+    SUM(CASE
+        WHEN a.description IN ('Assurance internationale', 'Assurance étudiante') THEN f.montant
+        ELSE f.montant * 0.5
+    END) AS MontantAssuré
+FROM Facture AS f
+JOIN Assurance AS a ON f.id_facture = a.id_facture
+JOIN Patient AS p ON f.id_patient = p.id_patient
+GROUP BY p.NomComplet;
+
+--- 5.1.4
+SELECT
+    DATE_FORMAT(date, '%Y-%m') AS MoisDeLUrgence,
+    COUNT(id_urgence) AS NombreTotalUrgences
 FROM
-  Patient
+    Urgence
+GROUP BY
+    MoisDeLUrgence
+ORDER BY
+    NombreTotalUrgences DESC;
+
+--- 5.1.5
+SELECT
+    p.NomComplet,
+    a.traitements,
+    COUNT(h.id_hospitalisation) AS NombreHospitalisationsRecentes
+FROM
+    Patient AS p
+JOIN
+    Profil_médical AS pm ON p.id_patient = pm.id_patient
+JOIN
+    Antécédents AS a ON pm.id_profil_médical = a.id_profil_médical
+JOIN
+    Hospitalisation AS h ON p.id_patient = h.id_patient
 WHERE
-  DATE_naissance > '2008-01-01';
 
-
-  
+    (a.traitements LIKE '%diabète%' OR a.traitements LIKE '%hypertension%')
+    AND h.date_admission >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
+GROUP BY
+    p.NomComplet, a.traitements
+HAVING
+    COUNT(h.id_hospitalisation) >= 2
+ORDER BY
+	NombreHospitalisationsRecentes DESC;
